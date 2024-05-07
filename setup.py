c9f43b2053fa0fd1d315235f7a7f086df4733364
chrisjrd <chris.jrd@gmail.com>
2024-05-07 15:32:25 -0700

    feat(accountability): dependencies
    Refs #826

--- setup.py
+++ setup.py
@@ -12,22 +12,24 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        "elasticsearch>=7.0.0,<8.0.0",
+        # periodically update the minimum versions of these with what's in the deployment environment
         "Flask>=2.2.5",
         "Flask-Compress>=1.15",
         "Flask-Cors>=4.0.1",
         "flask-restx>=1.3.0",
-        "more-itertools>=10.2.0",
-        "pandas==2.1.0",
 
-        "matplotlib==3.5.1",
+        "more-itertools>=10.2.0",
+        "elasticsearch>=7.13.4,<8.0.0",
+        "pandas>=2.1.0,== 2.*",
+        "matplotlib>=3.7.2",
     ],
     extras_require={
         'test': [
-            "pytest>=7.2.0",
-            "pytest-mock>=3.7.0",
+            "pytest>=7.4.2",
+            "pytest-mock",
             "coverage",
             "pytest-cov",
+
             "prov-es@https://github.com/hysds/prov_es/archive/refs/tags/v0.2.2.tar.gz",
             "osaka@https://github.com/hysds/osaka/archive/refs/tags/v1.1.0.tar.gz",
             "hysds-commons@https://github.com/hysds/hysds_commons/archive/refs/tags/v1.0.9.tar.gz",
